08d6ee973c5861786b87f1d1ea784515b03adb0e
jbapple <jbapple>
2013-06-08 11:09:59 -0700

    pair up implementations for tied benchmarking, meant to reduce ordering effeects

--- src/benchmark/insert-maxtime.cc
+++ src/benchmark/insert-maxtime.cc
@@ -3,12 +3,12 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
-#include "linear-probing.cc"
+#include <tuple>
+
+using namespace std;
+
+//#include "linear-probing.cc"
 #include "lazy-linear.cc"
-
-#include <sched.h>
-
-
 
 struct some {
   const static int some_size = 2;
@@ -50,43 +50,78 @@ size_t hash(const some & x) {
 
 typedef some sample_type;
 
-template<typename T>
-std::vector<std::pair<unsigned, double> > test(const unsigned size, const unsigned samples) {
+template<typename T, typename U>
+std::vector<std::tuple<unsigned, double, double> > test(const unsigned size, const unsigned samples) {
   unsigned i = 0;
-  std::vector<std::pair<unsigned, double> > ans;//(size * samples); 
-  //high_priority zz;
+  std::vector<std::tuple<unsigned, double, double> > ans((size * samples)); 
+  high_priority zz;
   for (unsigned k = 0; k < samples; ++k) { 
-    T playground;
-    double leader = 0.0;
+    T p;
+    U q;
+    double l1 = 0.0, l2 = 0.0;
     for (unsigned j = 0; j < size; ++j) { 
-      const auto start = get_time();
-      playground.insert(some::random());
-      const auto here = get_time() - start; 
-      leader = std::max(leader, here);
-      ans.push_back(make_pair(j,leader));
+      const auto x = some::random();
+      double begin, mid, end;
+      if (not (j & 1)) {
+        begin = get_time();
+        p.insert(x);
+        mid = get_time(); 
+        q.insert(x);
+        end = get_time();
+        l1 = std::max(l1, mid-begin);
+        l2 = std::max(l2, end-mid);
+      } else {
+        begin = get_time();
+        q.insert(x);
+        mid = get_time(); 
+        p.insert(x);
+        end = get_time();
+        l2 = std::max(l2, mid-begin);
+        l1 = std::max(l1, end-mid);
+      }
+      ans[i++] = make_tuple(j,l1,l2);
     } 
   }
   return ans;
 }
 
-void print_test(std::vector<std::pair<unsigned, double> > x) {
-  std::unordered_map<unsigned, std::vector<double> > collect;    
+void print_test(std::vector<std::tuple<unsigned, double, double> > x) {
+  std::unordered_map<unsigned, std::pair<std::vector<double>, std::vector<double> > > collect;    
   for (const auto& y : x) {
-    collect[y.first].push_back(y.second);
+    collect[get<0>(y)].first.push_back(get<1>(y));
+    collect[get<0>(y)].second.push_back(get<2>(y));
   }
   for (const auto& y : collect) {
-    std::cout << y.first << '\t' << avg(y.second) << std::endl;
+    std::cout << y.first << '\t' 
+              << avg(y.second.first) << '\t'
+              << avg(y.second.second) << std::endl;
   }
 }
 
 int main(int argc, char ** argv) {
   srand(0);
-  unsigned size = 100000;//00;
+  unsigned size = 10000;//00;
   unsigned samples = 1000;
   if (4 == argc) {
     size = read<unsigned>(argv[2]);
     samples = read<unsigned>(argv[3]);
   }
+
+  print_test(test<
+               //std::set<sample_type>, //, std::set<sample_type> >
+               quiet_map<sample_type, lazier_map<sample_type, BasicBitArray> >
+               ,
+               quiet_map<sample_type, lazier_map<sample_type, AhoBitArray> >
+
+                 >
+                 
+    (size, samples));
+  /*
+ >(size, samples));
+  } else if ("aho" == container_type_string) {
+    print_test(test<quiet_map<sample_type, lazier_map<sample_type, AhoBitArray> > >(size, samples));
+
+  
   const std::string container_type_string = argv[1];
   
   if ("tree" == container_type_string) {
@@ -104,5 +139,5 @@ int main(int argc, char ** argv) {
   } else {
     std::cerr << "No test";
   }
-
+  */
 }
--- src/benchmark/insert-maxtime.gnuplot
+++ src/benchmark/insert-maxtime.gnuplot
@@ -3,14 +3,16 @@ set title "Average Maximum Insert Time"
 set xlabel "Items"
 set ylabel "Seconds"
 #set xrange [1:100000]
-#set yrange [0.0000001:0.001]
+#set yrange [0:0.00002]
 set terminal png size 800, 600
 set output "insert-maxtime.png"
 set logscale y 10
 set logscale x 10
 plot \
-"./insert-maxtime_tree.dat" with lines lc rgb "green" lw 2 title "tree", \
-"./insert-maxtime_separate.dat" with lines lc rgb "purple" lw 2 title "separate hash table", \
-"./insert-maxtime_aho.dat" with lines lc rgb "orange" lw 2 title "aho hash table", \
-"./insert-maxtime_lazy.dat" with lines lc rgb "blue" lw 2 title "lazy hash table", \
-"./insert-maxtime_hash.dat" with lines lc rgb "red" lw 2 title "hash table"
+"insert-maxtime.dat" using 1:2 with lines lc rgb "red" lw 1 title "separate", \
+"insert-maxtime.dat" using 1:3 with lines lc rgb "blue" lw 1 title "aho"
+#"./insert-maxtime_tree.dat" with lines lc rgb "green" lw 2 title "tree", \
+#"./insert-maxtime_separate.dat" with lines lc rgb "purple" lw 2 title "separate hash table", \
+#"./insert-maxtime_aho.dat" with lines lc rgb "orange" lw 2 title "aho hash table"#, \
+#"./insert-maxtime_lazy.dat" with lines lc rgb "blue" lw 2 title "lazy hash table", \
+#"./insert-maxtime_hash.dat" with lines lc rgb "red" lw 2 title "hash table"
