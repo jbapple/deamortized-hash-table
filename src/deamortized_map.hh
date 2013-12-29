f1064012f89d8ad297897c7a0933002fa8d400c6
Jim Apple <jim@localhost.(none)>
2013-12-28 19:33:46 -0800

    Switch to AA trees for deamortized map

--- src/deamortized_map.hh
+++ src/deamortized_map.hh
@@ -1,85 +1,107 @@
+#include <cstddef>
+#include <utility>
+#include <cstdlib>
+
 template<typename Key, typename Val>
 struct deamortized_map {
-  deamortized_map();
-  struct Cell {
+  struct Node {
     Key key;
-    Value val;
+    Val val;
+    Node *left, *right; // tree structure
+    Node *prev, *next; // for iterating
+    std::size_t level; // for balance
+    Node(const Key& key, const Val& val) 
+      : key(key), val(val),
+	left(bottom()), right(bottom()), prev(NULL), next(NULL), level(1) {}
   };
-  Node {
-    Cell data;
-    Node *left, *right, *next;
-    size_t height;
-  };
 
-  Node * head;
-  Cell * find(const Key&) const;
-  Cell * insert(const Key&, const Val&);
-  size_t size;
-};
-
-template<typename Key, typename Val>
-struct insert_ans {
-  bool success;
-  typename deamortized_map<Key,Val>::Node *here, *ans;
-}
-
-template<typename Key, typename Val>
-insert_ans<Key,Val> node_insert(typename deamortized_map<Key,Val>::Node * root, const Key& key, const Val& val) {
-  if (root->right && root->right->right && root->right->right->height == root->height) {
-    typename deamortized_map<Key,Val>::Node * new_root = root->right;
-    root->right = new_root->left;
-    new_root->left = root;
-    new_root->height += 1;
-    return node_insert(new_root, key, val);
-  }
-  if (root) {
-    if (key == root->key) return {false, root, root};
-    if (key < root->key) return node_insert
-  }
-}
-
-// TODO: sentinel
-
-template<typename Key, typename Val>
-std::pair<bool,Node *> node_insert(typename deamortized_map<Key,Val>::Node * root, const Key& key, const Val& val) {
-  if (not root) {
-    auto * ans = new typename deamortized_map<Key,Val>::Node();
-    ans->data->key = key;
-    ans->left = ans->right = ans->next = nullptr;
-    ans->height = 0;
-    return std::make_pair(true, ans);
-  }
-  if (key == root->data->key) {
-    return std::make_pair(false, root);
-  }
-  if (key < root->data->key) {
-    if (left_heavy(root)) { root = rotate_right(root); }
-    root->left = node_insert(root->left, key, val).second;
-    reheight(root);
+  // AA trees:
+  static Node * skew(Node * root) {
+    if (root->left->level == root->level) {
+      // rotate right
+      Node * ans = root->left;
+      root->left = ans->right;
+      ans->right = root;
+      return ans;
+    }
     return root;
   }
-    
-}
 
-template<typename Key, typename Val>
-typename deamortized_map<Key,Val>::Node * rotate_right(typename deamortized_map<Key,Val>::Node * root) {
-  // single rotation:
-  if ((not root->left->left) or (not root->left->right) or (root->left->left->height >= root->left->right->height)) {
-    typename deamortized_map<Key,Val>::Node * const ans = root->left;
-    root->left = ans->right;
-    ans->right = root;
-    reheight(root);
-    reheight(ans);
-    return ans;
-  } else { // double rotation
-    typename deamortized_map<Key,Val>::Node * const ans = root->left->right;
-    root->left->right = ans->left;
-    ans->left = root->left;
-    root->left = ans->right;
-    ans->right = root;
-    reheight(ans->left);
-    reheight(ans->right);
-    reheight(ans);
+  static Node * split(Node * root) {
+    if (root->right->right->level == root->level) {
+      //rotate left
+      Node * ans = root->right;
+      root->right = ans->left;
+      ans->left = root;
+      ans->level += 1;
+      return ans;
+    }
+    return root;
+  }
+
+  struct InsertAns {
+    bool is_new;
+    Node * location;
+    Node * new_root;
+  };
+
+  static InsertAns insert(const Key& key, const Val& val, Node * root) {
+    if (0 == root->level) {
+      InsertAns ans;
+      // link up node later
+      ans.location = new Node(key, val);
+      ans.new_root = ans.location;
+      ans.is_new = true;
+      return ans;
+    }
+    InsertAns ans;
+    if (key < root->key) ans = insert(key, val, root->left);
+    else if (key < root->key) ans = insert(key, val, root->right);
+    else {
+      ans.is_new = false;
+      ans.location = root;
+      ans.new_root = root;
+    }
+    if (ans.is_new) {
+      ans.new_root = skew(ans.new_root);
+      ans.new_root = split(ans.new_root);
+    }
     return ans;
   }
-}
+
+  static Node * find(const Key& key, const Node* const root) {
+    if (0 == root->level) return NULL;
+    if (key <= root->key) return root;
+    if (key < root->key) return find(key, root->left);
+    return find(key, root->right);
+  }
+
+  Node *head, *root;
+  std::size_t size;
+  deamortized_map() : head(NULL), root(bottom()), size(0) {}
+  Node * find(const Key& key) const {
+    return find(key, root);
+  }
+  std::pair<bool,Node*> insert(const Key& key, const Val& val) {
+    InsertAns ia = insert(key, val, root);
+    if (ia.is_new) {
+      ia.location->next = head;
+      ia.location->prev = NULL;
+      head = ia.location;
+      root = ia.new_root;
+      ++size;
+    }
+    return make_pair(ia.is_new, ia.location);
+  }
+
+  static const Node * bottom() {
+    static Node * ans = NULL;
+    if (NULL == ans) {
+      ans = malloc(sizeof(Node));
+      ans->left = ans->right = ans;
+      ans->prev = ans->next = NULL;
+      ans->level = 0;
+    }
+    return ans;
+  }
+};
