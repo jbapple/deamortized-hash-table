7548378acad7fd5b5013966228ea76ba236c7b37
Jim Apple <jim@localhost.(none)>
2013-12-28 18:39:28 -0800

    First pass at a deamortized map

--- src/deamortized_map.hh
+++ src/deamortized_map.hh
@@ -2,63 +2,84 @@ template<typename Key, typename Val>
 struct deamortized_map {
   deamortized_map();
   struct Cell {
-    Key k;
-    Value v;
+    Key key;
+    Value val;
   };
-  KillNode {
-    Node * node;
-    KillNode * next;
-  };
-  KillNode kill_list;
   Node {
     Cell data;
-    Node *left, *right;
+    Node *left, *right, *next;
     size_t height;
   };
+
+  Node * head;
   Cell * find(const Key&) const;
   Cell * insert(const Key&, const Val&);
   size_t size;
-
-  // simpler idea: track the order the keys came in!
-
-  Cursor {
-    Node * node;
-    Cursor * up;
-    // DFS in O(1) worst-case time
-    bool advance() {
-      if (not node) return false;
-      if (not node->left) {
-	if (not node->right) {
-	  if (not up) return false;
-	  node = up->node->right;
-	  up = up->up;
-	  return advance();
-	}
-	node = node->right;
-	
-	  
-    }
-  };
-
-
-  void kill_one() {
-    Node *left_orphan = kill_list.node->left, 
-      *right_orphan = kill_list.node->right;
-    delete kill_list->node;
-    if (right_orphan and not left_orphan) { std::swap(left_orphan, right_orphan); }
-    if (right_orphan) {
-      KillNode * head = new KillNode();
-      head->node = right_orphan;
-      head->next = kill_list->next;
-      kill_list->next = head;
-      kill_list->node = left_orphan;
-    } else if (left_orphan) {
-      kill_list->node = left_orphan;
-    } else if (kill_list->next) {
-      kill_list->node = kill_list->next->node;
-      kill_list->next = kill_list->next->next;
-      delete kill_list->next;
-    }
-    --size;
-  }
 };
+
+template<typename Key, typename Val>
+struct insert_ans {
+  bool success;
+  typename deamortized_map<Key,Val>::Node *here, *ans;
+}
+
+template<typename Key, typename Val>
+insert_ans<Key,Val> node_insert(typename deamortized_map<Key,Val>::Node * root, const Key& key, const Val& val) {
+  if (root->right && root->right->right && root->right->right->height == root->height) {
+    typename deamortized_map<Key,Val>::Node * new_root = root->right;
+    root->right = new_root->left;
+    new_root->left = root;
+    new_root->height += 1;
+    return node_insert(new_root, key, val);
+  }
+  if (root) {
+    if (key == root->key) return {false, root, root};
+    if (key < root->key) return node_insert
+  }
+}
+
+// TODO: sentinel
+
+template<typename Key, typename Val>
+std::pair<bool,Node *> node_insert(typename deamortized_map<Key,Val>::Node * root, const Key& key, const Val& val) {
+  if (not root) {
+    auto * ans = new typename deamortized_map<Key,Val>::Node();
+    ans->data->key = key;
+    ans->left = ans->right = ans->next = nullptr;
+    ans->height = 0;
+    return std::make_pair(true, ans);
+  }
+  if (key == root->data->key) {
+    return std::make_pair(false, root);
+  }
+  if (key < root->data->key) {
+    if (left_heavy(root)) { root = rotate_right(root); }
+    root->left = node_insert(root->left, key, val).second;
+    reheight(root);
+    return root;
+  }
+    
+}
+
+template<typename Key, typename Val>
+typename deamortized_map<Key,Val>::Node * rotate_right(typename deamortized_map<Key,Val>::Node * root) {
+  // single rotation:
+  if ((not root->left->left) or (not root->left->right) or (root->left->left->height >= root->left->right->height)) {
+    typename deamortized_map<Key,Val>::Node * const ans = root->left;
+    root->left = ans->right;
+    ans->right = root;
+    reheight(root);
+    reheight(ans);
+    return ans;
+  } else { // double rotation
+    typename deamortized_map<Key,Val>::Node * const ans = root->left->right;
+    root->left->right = ans->left;
+    ans->left = root->left;
+    root->left = ans->right;
+    ans->right = root;
+    reheight(ans->left);
+    reheight(ans->right);
+    reheight(ans);
+    return ans;
+  }
+}
